6887f944cb4e3c6f781484b0aba394bc9a22dbc9
Mir Ahmad Shah <153583051+99Ahmadprojects@users.noreply.github.com>
2025-09-17 18:30:39 +0500

    Initial commit

---
+++ lib/dart_basics.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter your name: ");
+  String? name = stdin.readLineSync();
+
+  stdout.write("Enter your age: ");
+  int age = int.parse(stdin.readLineSync()!);
+
+  if (age < 18) {
+    print("Sorry $name, you are not eligible to register.");
+    return;
+  }
+
+  stdout.write("How many numbers do you want to enter? ");
+  int n = int.parse(stdin.readLineSync()!);
+
+  List<int> numbers = [];
+
+  for (int i = 0; i < n; i++) {
+    stdout.write("Enter number ${i + 1}: ");
+    int num = int.parse(stdin.readLineSync()!);
+    numbers.add(num);
+  }
+
+  int evenSum = 0;
+  int oddSum = 0;
+
+  for (int num in numbers) {
+    if (num % 2 == 0) {
+      evenSum += num;
+    } else {
+      oddSum += num;
+    }
+  }
+
+  int largest = numbers.reduce((a, b) => a > b ? a : b);
+  int smallest = numbers.reduce((a, b) => a < b ? a : b);
+
+  print("\n--- Results ---");
+  print("Numbers entered: $numbers");
+  print("Sum of even numbers: $evenSum");
+  print("Sum of odd numbers: $oddSum");
+  print("Largest number: $largest");
+  print("Smallest number: $smallest");
+}
